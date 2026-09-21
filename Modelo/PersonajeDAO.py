4e572a01c247c16829d790f8e363ce358c6e5ea6
Samuel <samuelestupinan414@gmail.com>
2026-09-21 17:14:47 -0500

    Se agrego el apartado resouces y un archivo personaje.html para mostrar datos de los personajes existentes y los personajes agregados, en este archivo esta el formulario para agregar los personajes.

--- Modelo/PersonajeDAO.py
+++ Modelo/PersonajeDAO.py
@@ -6,3 +6,6 @@ class PersonajeDAO:
     def obtener_todos(self):
         Personajes_dict = [personaje.to_dict() for personaje in self.personajes]
         return Personajes_dict
+
+    def agregar_personaje(self, personaje):
+        self.personajes.append(personaje)
--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, redirect, request, render_template
 import Modelo.PersonajeDAO as personaje_dao
+from Modelo.Personaje import Personaje
 
 app = Flask(__name__)
 personaje_dao = personaje_dao.PersonajeDAO()
@@ -7,7 +8,18 @@ personaje_dao = personaje_dao.PersonajeDAO()
 @app.route('/personajes', methods=['GET'])
 def obtener_personajes():
     personajes = personaje_dao.obtener_todos()
-    return jsonify(personajes)
+    return render_template('personaje.html', personajes=personajes)
+
+@app.route('/crear-personaje', methods=['POST'])
+def crear_personaje():
+    nombre = request.form.get('nombre')
+    clase = request.form.get('clase')
+
+    nuevo_personaje = Personaje(nombre, clase, nivel=1, vida=100)
+    
+    personaje_dao.agregar_personaje(nuevo_personaje)
+
+    return redirect('/personajes')
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
